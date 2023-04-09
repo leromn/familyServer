02323a7482a891b25a4a3d0022e892aa75d0b223
CodeSandbox <support@codesandbox.io>
2023-04-09 04:28:35 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const server = new express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const formidable = require("formidable");
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(cors());
+
+server.post("/", (req, res) => {
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+    console.log("fields: ", fields);
+    console.log("files: ", files);
+    res.send({ success: true });
+  });
+});
+
+server.listen(8080, () => {
+  console.log("listning on port");
+});
